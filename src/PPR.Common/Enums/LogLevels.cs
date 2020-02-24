e9cd19775687f99c8a235a2c4318179da0c5865a
patrix <patrix@example.com>
2020-02-24 08:00:56 +0100

    Initial implementation of Common Layer

---
+++ src/PPR.Common/Enums/LogLevels.cs
@@ -0,0 +1,24 @@
+namespace PPR.Common.Enums {
+    public enum LogLevels {
+        /// <summary>
+        /// The debug
+        /// </summary>
+        Debug,
+        /// <summary>
+        /// The error
+        /// </summary>
+        Error,
+        /// <summary>
+        /// The fatal
+        /// </summary>
+        Fatal,
+        /// <summary>
+        /// The information
+        /// </summary>
+        Information,
+        /// <summary>
+        /// The warning
+        /// </summary>
+        Warning
+    }
+}
---
+++ src/PPR.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PPR.Data
+{
+    public class Class1
+    {
+    }
+}
